5cddc8c838d444f421c18ca726dde2bf439f5027
robbiemartinus <robbie.martinus@yahoo.com.au>
2017-03-07 07:24:59 +1100

    use promise

--- modules/google-place.js
+++ modules/google-place.js
@@ -1,30 +1,28 @@
 var https = require('https');
 
-exports.search = function(apiKey, query, callback) {
-    //return new Promise((resolve, reject) => {
+exports.search = function(apiKey, query) {
+    return new Promise((resolve, reject) => {
         console.log('searching with apiKey ' + apiKey + ' and query ' + query);
         var url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + apiKey;
         https.get(url, function (response) {
-                    var body = '';
+            var body = '';
 
-                    response.on('data', function (chunk) {
-                        body += chunk;
-                    });
+            response.on('data', function (chunk) {
+                body += chunk;
+            });
 
-                    response.on('end', function () {
-                        var places = JSON.parse(body);
-                        if (places.status == "OK") {
-                        } else {
-                            console.log("Query is invalid: ", query);
-                        }
-                        //resolve(places);
-                        callback(null, places);
-                    });
-
-                }).on('error', function (e) {
-                    console.log("Got error: " + e.message);
-                    //reject(err);
-                    callback(err, "error");
-                });
-   //});
+            response.on('end', function () {
+                var places = JSON.parse(body);
+                if (places.status == "OK") {
+                    resolve(places);
+                } else {
+                    console.log("Query is invalid: ", query);
+                    reject(places);
+                }
+            });
+        }).on('error', function (e) {
+            console.log("Got error: " + e.message);
+            reject(err);
+        });
+   });
 }
--- test/google-place-test.js
+++ test/google-place-test.js
@@ -1,34 +1,35 @@
 var chai = require('chai');
 var expect = chai.expect;
-//var chaiAsPromised = require('chai-as-promised');
-//chai.use(chaiAsPromised);
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 var sinon = require('sinon');
+//require('sinon-as-promised')
 var googlePlace = require('../modules/google-place.js');
 var PassThrough = require('stream').PassThrough;
 var https = require('https');
 
-describe('Google Place', function() {
-    beforeEach(function() {
+describe('Google Place', () => {
+    beforeEach(() => {
 		this.request = sinon.stub(https, 'get');
 	});
 
-    it('should return the same value as place search result', function() {
+    it('should return the same value as place search result', () => {
         var expected = { status: 'OK' };
-        var response = new PassThrough();
-        response.write(JSON.stringify(expected));
-        response.end();
+
+        var httpsResponse = new PassThrough();
+        httpsResponse.write(JSON.stringify(expected));
+        httpsResponse.end();
 
         var request = new PassThrough();
 
-        this.request.callsArgWith(1, response)
+        this.request.callsArgWith(1, httpsResponse)
                     .returns(request);
 
-        googlePlace.search('a', 'b', function(err, result) {
-            expect(result).to.deep.equal(expected);
-        });
+        var result = googlePlace.search('a', 'b');
+        expect(result).to.eventually.deep.equal(expected);
     });
 
-    afterEach(function() {
+    afterEach(() => {
         https.get.restore();
     });
 });
